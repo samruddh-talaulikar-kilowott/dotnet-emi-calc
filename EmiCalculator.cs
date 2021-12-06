ba46b1a0248d68afc1a1250838183acc489418bb
Samruddh Talaulikar <samruddh.talaulikar@kilowott.com>
2021-12-06 13:28:38 +0530

    Commit

---
+++ EmiCalculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotnetemicalc
+{
+    internal class EmiCalculator
+    {
+        public double CalculateEmi(double p, double i, double t, double n)
+        {
+            double emi;
+            emi = p * Math.Pow((1 + i / n), (t * n));
+            return (emi) / 24;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,30 +2,34 @@
 namespace dotnetemicalc
 {
     internal class Program
-    {
-        internal class Calc
+    { 
+        public static void Main(string[] args)
         {
-            public double EmiCalculator(double p, double i, double t, double n)
-            {
-                double emi;
-                emi = p * Math.Pow((1 + i / n), (t * n));
-                return (emi) / 24;
-            }
-        }
-        static void Main(string[] args)
-        {
-            var sp = new Calc();
             double p, i, n, t, emi;
-            Console.WriteLine("Enter principle:");
-            p = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Enter interest rate");
-            i = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Enter loan duration");
-            t = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Enter monthly compounding");
-            n = Convert.ToDouble(Console.ReadLine());
-            emi = sp.EmiCalculator(p, i, t, n);
-            Console.WriteLine("Monthly EMI is: " + Math.Round(emi, 2) + " INR");
+            var sp = new EmiCalculator();
+            try
+            {
+                Console.WriteLine("Enter principle:");
+                p = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Enter interest rate");
+                i = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Enter loan duration");
+                t = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Enter monthly compounding");
+                n = Convert.ToDouble(Console.ReadLine());
+                emi = sp.CalculateEmi(p, i, t, n);
+                Console.WriteLine("Monthly EMI is: " + Math.Round(emi, 2) + " INR");
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Invalid Input!");
+            }
+            /*
+            finally
+            {
+                Console.WriteLine("EndOfCode");
+            }
+            */
         }
     }
 }

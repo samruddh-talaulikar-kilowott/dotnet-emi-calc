19f1ca33da3592b1a927622843118a6745d74357
Samruddh Talaulikar <92842223+samruddh-talaulikar-kilowott@users.noreply.github.com>
2021-12-09 17:49:18 +0530

    Commit

---
+++ EMIAPP/Controllers/ContinuosEmiController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using EMIAPP.Models;
+namespace EMIAPP.Controllers
+{
+    public class ContinuosEmiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ContinuosEmi()
+        {
+            return View();
+        }
+
+        public IActionResult Output(double principal, double rate, double duration)
+        {
+            Request request = new Request();
+            request.Principal = principal;
+            request.InterestRateInPercentage = rate;
+            request.LoanDurationInYearCount = duration;
+            double e = 2.7183;
+
+            double Result = (double)request.Principal * System.Math.Pow(e, ((request.InterestRateInPercentage/100) * request.LoanDurationInYearCount)) ;
+
+
+            request.Result = Result;
+            return View(request);
+
+
+        }
+    }
+}

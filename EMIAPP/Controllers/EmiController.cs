c39f215779d0ced255aa676b10ca767bd3b5bb0b
Samruddh Talaulikar <92842223+samruddh-talaulikar-kilowott@users.noreply.github.com>
2021-12-09 19:40:46 +0530

    Commit

---
+++ EMIAPP/Controllers/EmiController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using EMIAPP.Models;
+namespace EMIAPP.Controllers
+{
+    public class EmiController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+
+        public IActionResult MonthlyEmi()
+        {
+            return View();
+        }
+
+        public IActionResult Output(double principal, double rate, double duration)
+        {
+
+            Request request = new Request();
+            request.Principal = principal;
+            request.InterestRateInPercentage = rate;
+            request.LoanDurationInYearCount = duration;
+
+
+            double MonthlyEmi = (double)request.Principal * System.Math.Pow(1 + (request.InterestRateInPercentage / 100) / 12, request.LoanDurationInYearCount * 12);
+            double e = 2.7183;
+            double ContinousEmi = (double)request.Principal * System.Math.Pow(e, (request.InterestRateInPercentage / 100 * request.LoanDurationInYearCount));
+            double DailyEmi = ((double)request.Principal * System.Math.Pow(1 + ((request.InterestRateInPercentage / 100) / 365), 365 * request.LoanDurationInYearCount)) - (double)request.Principal;
+
+            request.MonthlyEmi = MonthlyEmi;
+            request.ContinousEmi = ContinousEmi;
+            request.DailyEmi = DailyEmi;
+
+            return View(request);
+
+
+        }
+    }
+
+}
---
+++ EMIAPP/Models/Request.cs
@@ -0,0 +1,12 @@
+﻿namespace EMIAPP.Models
+{
+    public class Request
+    {
+        public double Principal { internal get; set; }
+        public double InterestRateInPercentage { internal get; set; }
+        public double LoanDurationInYearCount { internal get; set; }
+        public double MonthlyEmi { internal get; set; }
+        public double DailyEmi { internal get; set; }
+        public double ContinousEmi { internal get; set; }
+    }
+}

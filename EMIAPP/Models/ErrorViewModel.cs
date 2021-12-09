11a92a9ed9f89f4485f3970b4baaba0f59914f10
Samruddh Talaulikar <92842223+samruddh-talaulikar-kilowott@users.noreply.github.com>
2021-12-09 17:19:36 +0530

    Commit

---
+++ EMIAPP/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EMIAPP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ EMIAPP/Models/Request.cs
@@ -0,0 +1,10 @@
+﻿namespace EMIAPP.Models
+{
+    public class Request
+    {
+        public double Principal { internal get; set; }
+        public double InterestRateInPercentage { internal get; set; }
+        public double LoanDurationInYearCount { internal get; set; }
+        public double Result { internal get; set; }
+    }
+}
